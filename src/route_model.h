ee95043df9c52bfb41c68f0c378fbc9e79135f86
kentyucra <mckkentmwk@gmail.com>
2022-10-07 14:58:01 +0000

    first commit

---
+++ src/route_model.h
@@ -0,0 +1,60 @@
+#ifndef ROUTE_MODEL_H
+#define ROUTE_MODEL_H
+
+#include <limits>
+#include <cmath>
+#include <unordered_map>
+#include "model.h"
+#include <iostream>
+#define print(x) std::cout << #x" = " << x << std::endl
+
+
+class RouteModel : public Model {
+
+  public:
+    class Node : public Model::Node {
+      public:
+        Node * parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        std::vector<Node *> neighbors;
+
+        void FindNeighbors();
+        float distance(Node other) const {
+            return std::sqrt(std::pow((x - other.x), 2) + std::pow((y - other.y), 2));
+        }
+
+        void PrintModelNode() {
+
+          std::cout << "Printing Mode Node ................" << std::endl;          
+          print(x);
+          print(y);
+          print(h_value);
+          print(g_value);
+          print(visited); 
+          std::cout << "//////////////////////////////////////////////" << std::endl;
+        }
+
+        Node(){}
+        Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
+
+      private:
+        int index;
+        Node * FindNeighbor(std::vector<int> node_indices);
+        RouteModel * parent_model = nullptr;
+    };
+
+    RouteModel(const std::vector<std::byte> &xml);
+    Node &FindClosestNode(float x, float y);
+    auto &SNodes() { return m_Nodes; }
+    std::vector<Node> path;
+    
+  private:
+    void CreateNodeToRoadHashmap();
+    std::unordered_map<int, std::vector<const Model::Road *>> node_to_road;
+    std::vector<Node> m_Nodes;
+
+};
+
+#endif
